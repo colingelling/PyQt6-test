f36df84ae79d61ecd22cfe8582d5dc70124d89fe
Colin Gelling <colingelling96@gmail.com>
2023-02-02 11:50:07 +0100

    Mostly added description

--- views/home_view.py
+++ views/home_view.py
@@ -4,11 +4,8 @@ Created by Colin Gelling on 30/1/2023
 
 """
 
-import sys
-
 from PyQt6 import QtCore, QtWidgets
 from PyQt6.QtGui import QAction
-from PyQt6.QtWidgets import QApplication, QPushButton
 
 import core.Models.Views.HomeModel as HomeModel
 
@@ -40,7 +37,31 @@ class HomeView(QtWidgets.QWidget):
 
         button_home = QAction("Home", self)
         button_home.triggered.connect(self.refer_home_route)
-        # TODO: fix error -> AttributeError: module 'core.Action.Controllers.HomeController' has no attribute 'HomeController'
+
+        """
+        TODO: 
+
+        1. Fix error -> AttributeError: module 'core.Action.Controllers.HomeController' has no attribute 'HomeController'
+        2. As of feb/1, this description has been returned without any error
+
+        What changed? Reference (https://github.com/colingelling/PyQt6-test/blob/main/views/home_view.py)
+        - Compare the main.py from this version to the main.py from https://github.com/colingelling/PyQt6-test and check 
+        the bottom of the reference mentioned above this line
+        
+        Also, navigating between windows doesn't work properly as of now; 
+        - Going from Home (launching state) to Login, to Register back to Login or either, closes the application 
+        instead of keeping the event loop going
+        - When clicking on the Home tab the application almost always closes instead of:
+        1. Staying within the Home Window when triggered from the same window
+        2. Returning to the Home window when triggered from another window
+        
+        Frank and Leon advised me to look into Event Handling more, maybe the first started Event 
+        (According to Home route starting within Bootstrapper.py) must be closed before returning to it later.
+        
+        Also Leon mentioned that the application has been started within the main, maybe that each window is going to be 
+        setup the same way as it did for the first launch. 
+
+        """
 
         button_login = QAction("Login", self)
         button_login.triggered.connect(self.refer_login_route)
@@ -48,13 +69,9 @@ class HomeView(QtWidgets.QWidget):
         button_register = QAction("Register", self)
         button_register.triggered.connect(self.refer_register_route)
 
-        # button_register.triggered.signal(lambda: self.close())
-
         menubar.addAction(button_home)
         menubar.addAction(button_login)
         menubar.addAction(button_register)
-
-        # menubar.addAction(trigger_login)
 
         # Navigation end
 
--- views/register_view.py
+++ views/register_view.py
@@ -36,7 +36,32 @@ class RegisterView(QtWidgets.QWidget):
         menubar = self.view_ui.register_ui.menuBar
 
         button_home = QAction("Home", self)
-        button_home.triggered.connect(self.refer_home_route)  # TODO: fix error -> AttributeError: module 'core.Action.Controllers.HomeController' has no attribute 'HomeController'
+        button_home.triggered.connect(self.refer_home_route)
+
+        """
+        TODO: 
+
+        1. Fix error -> AttributeError: module 'core.Action.Controllers.HomeController' has no attribute 'HomeController'
+        2. As of feb/1, this description has been returned without any error
+
+        What changed? Reference (https://github.com/colingelling/PyQt6-test/blob/main/views/home_view.py)
+        - Compare the main.py from this version to the main.py from https://github.com/colingelling/PyQt6-test and check 
+        the bottom of the reference mentioned above this line
+        
+        Also, navigating between windows doesn't work properly as of now; 
+        - Going from Home (launching state) to Login, to Register back to Login or either, closes the application 
+        instead of keeping the event loop going
+        - When clicking on the Home tab the application almost always closes instead of:
+        1. Staying within the Home Window when triggered from the same window
+        2. Returning to the Home window when triggered from another window
+        
+        Frank and Leon advised me to look into Event Handling more, maybe the first started Event 
+        (According to Home route starting within Bootstrapper.py) must be closed before returning to it later.
+        
+        Also Leon mentioned that the application has been started within the main, maybe that each window is going to be 
+        setup the same way as it did for the first launch. 
+
+        """
 
         button_login = QAction("Login", self)
         button_login.triggered.connect(self.refer_login_route)
