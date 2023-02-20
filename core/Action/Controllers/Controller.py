0626a1dc1b4a75acb5ff92d1fd53451e60c90a57
Colin Gelling <colingelling96@gmail.com>
2023-02-20 10:24:57 +0100

    Separated Controller classes into one class one file

---
+++ core/Action/Controllers/Controller.py
@@ -0,0 +1,57 @@
+class Controller:
+
+    def __init__(self):
+        pass
+
+    # Instantiate the window Classes per method from now on, bind the windows to individual signals and show the window
+
+    def show_main_window(self):
+        from views.home_view import HomeView
+        self.main_window = HomeView()
+        self.main_window.switch_second.connect(self.show_second_window)
+        self.main_window.switch_third.connect(self.show_third_window)
+        self.main_window.show()
+
+        from views.register_view import RegisterView
+        self.second_window = RegisterView()
+        if self.second_window.isVisible():
+            self.second_window.hide()
+
+        from views.login_view import LoginView
+        self.third_window = LoginView()
+        if self.third_window.isVisible():
+            self.third_window.hide()
+
+    def show_second_window(self):
+        from views.register_view import RegisterView
+        self.second_window = RegisterView()
+        self.second_window.switch_first.connect(self.show_main_window)
+        self.second_window.switch_third.connect(self.show_third_window)
+        self.second_window.show()
+
+        from views.home_view import HomeView
+        self.main_window = HomeView()
+        if self.main_window.isVisible():
+            self.main_window.hide()
+
+        from views.login_view import LoginView
+        self.third_window = LoginView()
+        if self.third_window.isVisible():
+            self.third_window.hide()
+
+    def show_third_window(self):
+        from views.login_view import LoginView
+        self.third_window = LoginView()
+        self.third_window.switch_first.connect(self.show_main_window)
+        self.third_window.switch_second.connect(self.show_second_window)
+        self.third_window.show()
+
+        from views.home_view import HomeView
+        self.main_window = HomeView()
+        if self.main_window.isVisible():
+            self.main_window.hide()
+
+        from views.register_view import RegisterView
+        self.second_window = RegisterView()
+        if self.second_window.isVisible():
+            self.second_window.hide()
--- main.py
+++ main.py
@@ -5,16 +5,13 @@ Created by Colin Gelling on 30/1/2023
 """
 
 from PyQt6 import QtWidgets
-from core.Action.Controllers.HomeController import HomeController
+from core.Action.Controllers.Controller import Controller
 
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    from views.home_view import Controller
-    window = Controller()
-    window.show_main_window()
-    # controller = HomeController()
-    # controller.show_window()
+    controller = Controller()
+    controller.show_main_window()
     sys.exit(app.exec())
 
 
